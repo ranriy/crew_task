0f352fa4ee13a60f30b08159347a9f071836ad7e
ranriy <riyawasnik@gmail.com>
2020-07-23 18:34:38 -0400

    to_ical implementation

--- ruby_edition/cm_challenge/absences.rb
+++ ruby_edition/cm_challenge/absences.rb
@@ -1,9 +1,25 @@
+require 'icalendar'
 require_relative './api'
 
 module CmChallenge
   class Absences
-    def to_ical
-      # Your implementation here
-    end
+      def to_ical
+        cal = Icalendar::Calendar.new
+        CmChallenge::Api.absences.each do |record|
+            cal.event do |e|
+              e.dtstart     = Icalendar::Values::Date.new(record[:start_date].tr('-',''))
+              e.dtend       = Icalendar::Values::Date.new(record[:end_date].tr('-',''))
+              e.summary     = summary(record[:user_id],record[:type])
+              e.description = record[:member_note]
+              e.ip_class    = "PRIVATE"
+            end
+        end
+        cal.publish
+      end
+
+      def summary(user_id,type)
+        member_record = CmChallenge::Api.members.find { |record| record[:user_id]==user_id } 
+        type == "vacation" ? member_record[:name] + " is on vacation" : member_record[:name] + " is sick"
+      end
   end
 end
